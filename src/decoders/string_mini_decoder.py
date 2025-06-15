de8692ccbacd8df85aebd640294e4c36e1bf9cd5
TimDu38 <tim.leroux.74@gmail.com>
2025-06-15 03:12:53 +0200

    Optimized String mini conversion mode (faster decoding/rendering)

--- src/decoders/string_mini_decoder.py
+++ src/decoders/string_mini_decoder.py
@@ -1,24 +1,18 @@
 from kandinsky import fill_rect
 
 def show_image(palette, rectangle_string, pos=(0, 0)):
-    def decode_color(color):
-        r = ((ord(color[0]) - 35) * 255 + 15) // 31
-        g = ((ord(color[1]) - 35) * 255 + 31) // 63
-        b = ((ord(color[2]) - 35) * 255 + 15) // 31
-        return (r, g, b)
-    
-    def decode_value(string):
-            return ord(string) - 35
-        
-    x, y, w, h, c, f = pos[0], pos[1], 0, 0, 0, 0
+    x, y, w, h, c, f = 0, 0, 0, 0, 0, 0
+    px, py = pos
+    px, py = px - 35, py - 35
+    o = ord
     for i in rectangle_string:
-        v = decode_value(i)
+        v = o(i)
         if f == 4:
-            if v < 0:
+            if v < 35:
                 f = 5
                 continue
             else:
-                fill_rect(x + pos[0], y + pos[1], w, h, color)
+                fill_rect(x + px, y + py, w - 35, h - 35, color)
                 f = 0
         if f == 0:
              x = v
@@ -29,11 +23,11 @@ def show_image(palette, rectangle_string, pos=(0, 0)):
         elif f == 3:
              h = v
         elif f == 5:
-            c = v
-            palette_index = c * 3
-            color = decode_color(palette[palette_index:palette_index + 3])
+            c = (v - 35) * 3
+            color_str = palette[c:c + 3]
+            color = (((ord(color_str[0]) - 35) * 255 + 15) // 31, ((ord(color_str[1]) - 35) * 255 + 31) // 63, ((ord(color_str[2]) - 35) * 255 + 15) // 31)
         if f > 3:
-            fill_rect(x + pos[0], y + pos[1], w, h, color)
+            fill_rect(x + px, y + py, w - 35, h - 35, color)
             f = 0
             continue
         f += 1
