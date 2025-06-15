be2c22af6b461367a4a36a65e140576526e37575
TimDu38 <tim.leroux.74@gmail.com>
2025-06-16 01:15:48 +0200

    Optimized even, even further String mini conversion mode

--- src/decoders/string_mini_decoder.py
+++ src/decoders/string_mini_decoder.py
@@ -3,7 +3,7 @@ from kandinsky import fill_rect
 def show_image(palette, rectangle_string, pos=(0, 0)):
     draw = fill_rect
     pal = palette
-    x, y, w, h, c, f = 0, 0, 0, 0, 0, 0
+    f,x,y,w,h,color = 4, 0, 0, 0, 0, (0, 0, 0)
     px, py = pos
     px, py = px - 35, py - 35
     o = ord
@@ -15,22 +15,26 @@ def show_image(palette, rectangle_string, pos=(0, 0)):
                 continue
             else:
                 draw(x + px, y + py, w - 35, h - 35, color)
-                f = 0
-        if f == 0:
-             x = v
-        elif f == 1:
-             y = v
-        elif f == 2:
-             w = v
-        elif f == 3:
-             h = v
-        elif f == 5:
+                f = 1
+                x = v
+                continue
+        if f == 1:
+            y = v
+            f = 2
+            continue
+        if f == 2:
+            w = v
+            f = 3
+            continue
+        if f == 3:
+            h = v
+            f = 4
+            continue
+        if f == 5:
             c = v * 3 - 105
             color_str = pal[c:c + 3]
             color = ((ord(color_str[0]) * 255 - 8910) // 31, (ord(color_str[1]) * 255 - 8894) // 63, (ord(color_str[2]) * 255 - 8910) // 31)
             draw(x + px, y + py, w - 35, h - 35, color)
-            f = 0
-            continue
-        f += 1
+            f = 4
         
         
