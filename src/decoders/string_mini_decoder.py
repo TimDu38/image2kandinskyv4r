cf4107c24ea893e9a38b5b575968c047f12d9b04
TimDu38 <tim.leroux.74@gmail.com>
2026-04-23 21:54:55 +0200

    refactor: streamline show_image function and enhance UI with ttk widgets

--- src/decoders/string_mini_decoder.py
+++ src/decoders/string_mini_decoder.py
@@ -1,40 +1,18 @@
 from kandinsky import fill_rect
-
-def show_image(palette, rectangle_string, pos=(0, 0)):
-    draw = fill_rect
-    pal = palette
-    f,x,y,w,h,color = 4, 0, 0, 0, 0, (0, 0, 0)
-    px, py = pos
-    px, py = px - 35, py - 35
-    o = ord
-    for i in rectangle_string:
-        v = o(i)
-        if f == 4:
-            if v < 35:
-                f = 5
-                continue
-            else:
-                draw(x + px, y + py, w - 35, h - 35, color)
-                f = 1
-                x = v
-                continue
-        if f == 1:
-            y = v
-            f = 2
-            continue
-        if f == 2:
-            w = v
-            f = 3
-            continue
-        if f == 3:
-            h = v
-            f = 4
-            continue
-        if f == 5:
-            c = v * 3 - 105
-            color_str = pal[c:c + 3]
-            color = ((ord(color_str[0]) * 255 - 8910) // 31, (ord(color_str[1]) * 255 - 8894) // 63, (ord(color_str[2]) * 255 - 8910) // 31)
-            draw(x + px, y + py, w - 35, h - 35, color)
-            f = 4
-        
-        
+def show_image(pal,rects,pos=(0,0)):
+ d=fill_rect;o=ord;p=pal
+ f,x,y,w,h,c=4,0,0,0,0,(0,0,0)
+ a,b=pos;a-=35;b-=35
+ for i in rects:
+  v=o(i)
+  if f==4:
+   if v<35:f=5;continue
+   else:d(x+a,y+b,w-35,h-35,c);f=1;x=v;continue
+  if f==1:y=v;f=2;continue
+  if f==2:w=v;f=3;continue
+  if f==3:h=v;f=4;continue
+  if f==5:
+   q=v*3-105
+   z=p[q:q+3]
+   c=((o(z[0])*255-8910)//31,(o(z[1])*255-8894)//63,(o(z[2])*255-8910)//31)
+   d(x+a,y+b,w-35,h-35,c);f=4
